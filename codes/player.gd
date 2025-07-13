327da25f95892183a89b9f87cb76f1af955a668c
Ran <razaanarjuna@gmail.com>
2025-07-13 21:51:47 +0700

    Adding attack & some SFX

--- codes/player.gd
+++ codes/player.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 const SPEED = 180
 const JUMP_VELOCITY = -300.0
-@onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var sword = $Sword
+@onready var collision_shape_2d = $Sword/CollisionShape2D
+@onready var player_anim = $PlayerAnim
+@onready var slime_anim = $SlimeAnim
+@onready var attack_sfx = $AttackSfx
+@onready var jump_sfx = $JumpSfx
 
 var jumps_left : int = 0
 const Total_Jumps : int = 2
@@ -11,16 +15,16 @@ var is_attacking = false
 
 func _ready():
 	sword.monitoring = false  # Disable sword hitbox by default
+	$Sword.body_entered.connect(_on_sword_body_entered)
+	
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	if not is_attacking:
 		movement()
-	
-	update_sword_position()
 	
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		attack()
@@ -31,13 +35,15 @@ func movement():
 		#velocity.y = JUMP_VELOCITY
 	
 	# Im thinking for making the double jump is accesiable after interact with the power up
-	# by making the if nested
+	# by making the if nested, and check the boolean
+	
 	# Handle double jump
 	if is_on_floor():
 		jumps_left = Total_Jumps
 	
 	if jumps_left > 0 && velocity.y >= 0.0:
 		if Input.is_action_just_pressed("jump"):
+			jump_sfx.play()
 			velocity.y = JUMP_VELOCITY
 			jumps_left -= 1 
 
@@ -45,19 +51,22 @@ func movement():
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	
-	# Flip the sprite
+	# Usable but suck as hell gonna change it later
+	# Flip the sprite & Update sword position based on sprite facing
 	if direction > 0:
-		animated_sprite_2d.flip_h = false
+		player_anim.flip_h = false
+		sword.position.x = 14 # To the right
 	elif direction < 0:
-		animated_sprite_2d.flip_h = true
+		player_anim.flip_h = true
+		sword.position.x = -14 # To the left
 	
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite_2d.play("idle")
+			player_anim.play("idle")
 		else:
-			animated_sprite_2d.play("run")
+			player_anim.play("run")
 	else:
-		animated_sprite_2d.play("jump")
+		player_anim.play("jump")
 	
 	# Apply movement
 	if direction:
@@ -71,17 +80,18 @@ func movement():
 func attack():
 	if is_on_floor():
 		is_attacking = true
-		animated_sprite_2d.play("attack")
+		player_anim.play("attack")
+		attack_sfx.play()
 		sword.monitoring = true
+		$Sword/CollisionShape2D.disabled = false
 		
 		await get_tree().create_timer(0.3).timeout
 		sword.monitoring = false
+		$Sword/CollisionShape2D.disabled = true
 		is_attacking = false
 
-# Usable but suck as hell gonna change it later
-# Update sword position based on sprite facing
-func update_sword_position():
-	if animated_sprite_2d.flip_h :
-		sword.position.x = -12 # To the left
-	else:
-		sword.position.x = 12 # To the right
+# For detect enemies collision 
+func _on_sword_body_entered(body):
+	if body.is_in_group("enemies"):
+		if body.has_method("take_damage"):
+			body.take_damage()
--- codes/slime.gd
+++ codes/slime.gd
@@ -3,16 +3,36 @@ extends CharacterBody2D
 const Speed = 50
 
 var direction = 1
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var slime_anim = $SlimeAnim
+@onready var collision_shape_2d = $CollisionShape2D
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
+@onready var death_sfx = $DeathSfx
+var is_dying = false
 
 func _process(delta):
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite_2d.flip_h = true
-	elif ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite_2d.flip_h = false
+	movement(delta)
+
+func take_damage(amount := 1):
+	if is_dying:
+		return
 	
-	position.x += direction * Speed * delta
+	is_dying = true
+	collision_shape_2d.disabled = true
+	direction = 0 # Stop the slime
+	slime_anim.play("death")
+	death_sfx.play()
+	
+	await slime_anim.animation_finished
+	queue_free()
+
+func movement(delta):
+		if ray_cast_right.is_colliding():
+			direction = -1
+			slime_anim.flip_h = true
+		elif ray_cast_left.is_colliding():
+			direction = 1
+			slime_anim.flip_h = false
+	
+		position.x += direction * Speed * delta
+	
