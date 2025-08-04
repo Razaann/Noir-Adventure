3564a83fe962da577b87e96b0ca969f1e1ebf6d6
Ran <razaanarjuna@gmail.com>
2025-08-04 19:53:47 +0700

    Adding Slime Enemy

---
+++ Codes/slime.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var health := 3
+@export var speed := 20
+@onready var slime_anim = $SlimeAnim
+
+var knockback_velocity = Vector2.ZERO
+var knockback_duration := 0.1
+var knockback_timer := 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	slime_anim.play("idle")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if knockback_timer > 0:
+		velocity = knockback_velocity
+		knockback_timer -= delta
+	else:
+		pass
+		#velocity = move_toward(velocity, Vector2.ZERO, 100 * delta)
+	
+	move_and_slide()
